f36ef8eab1d5c1cefc39890a6a542d64ec5f8cac
chenjieen <chenjieen2012@gmail.com>
2023-10-20 12:03:33 +0200

    Replace the query into its local language. Namely, search in German if the location is in Germany.

--- collect_data_migrant_churches_by_google.py
+++ collect_data_migrant_churches_by_google.py
@@ -1,12 +1,20 @@
 import requests
+from googletrans import Translator
 
 API_KEY = '***'
 CSE_ID = '***'
 ENDPOINT = "https://www.googleapis.com/customsearch/v1"
 
-def fetch_from_google_cse(query):
+translator = Translator()
+
+def fetch_from_google_cse(language, location, language_of_location="de"):
+    # Formulate the query using the two keywords
+    query_en = f"evangelical church, {language} in {location}"
+
+    query_translated = translator.translate(query_en, language_of_location)
+
     params = {
-        'q': query,
+        'q': query_translated.text,
         'key': API_KEY,
         'cx': CSE_ID
     }
@@ -16,8 +24,9 @@ def fetch_from_google_cse(query):
     return data
 
 # Test
-query = "Internationale Christliche Gemeinde Karlsruhe"
-results = fetch_from_google_cse(query)
+language = "German"
+location = "Rome"
+results = fetch_from_google_cse(language, location, "it")
 
 # Extracting and printing URLs from the results
 for result in results.get('items', []):
